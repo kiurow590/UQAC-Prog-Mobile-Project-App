22e9d8970efe65a4118b51353bebe3e3a262e45c
kiurow590 <kiurow.geek@gmail.com>
2025-02-11 22:47:11 -0500

    Ajout de la page de choix de jeui

--- app/src/main/java/com/example/uqac_progmob_project/GameChoose.kt
+++ app/src/main/java/com/example/uqac_progmob_project/GameChoose.kt
@@ -1,9 +1,9 @@
 package com.example.uqac_progmob_project
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.uqac_progmob_project.databinding.GameChooseBinding
-import com.example.uqac_progmob_project.databinding.BannerVersion1Binding
 
 class GameChoose : AppCompatActivity() {
     private lateinit var binding: GameChooseBinding
@@ -14,6 +14,12 @@ class GameChoose : AppCompatActivity() {
         binding = GameChooseBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Accéder aux éléments de la bannière
+        val backButton = binding.topBanner.backButton
 
+        // Configurer le bouton retour pour fermer l'activité en cours
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
