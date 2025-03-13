2814e5480675be68229b0c50f75abcdaffec0d65
kiurow590 <kiurow.geek@gmail.com>
2025-03-12 23:24:44 -0400

    Update de l'ecran d'explication du jeu' avec gestion des composable object

--- app/src/main/java/com/example/uqac_progmob_project/gameChoose/GameChoose.kt
+++ app/src/main/java/com/example/uqac_progmob_project/gameChoose/GameChoose.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.uqac_progmob_project.GameDetailActivity
 import com.example.uqac_progmob_project.GameHistoryActivity
 import com.example.uqac_progmob_project.R
 import androidx.compose.runtime.remember
